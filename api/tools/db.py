9068fdb870bc440f8d25210a6dd7daac5306faa1
YenChen <yenchen0416@gmail.com>
2020-09-19 17:41:37 +0800

    refactor: sql commit, stop ci .travis.yml file

--- api/tools/db.py
+++ api/tools/db.py
@@ -25,15 +25,6 @@ def get_colname(table: str, database: str):
     return query(f"SHOW COLUMNS FROM {table}", database)
 
 
-def is_where_or_and(first_condiction_flag):
-    if first_condiction_flag:
-        where_or_and = "WHERE"
-        first_condiction_flag = False
-    else:
-        where_or_and = "AND"
-    return first_condiction_flag, where_or_and
-
-
 def get_keywords_page_sql(
     colname: str,
     table: str,
@@ -51,51 +42,37 @@ def get_keywords_page_sql(
     sql = """
         SELECT {0}
         FROM `{1}`
+        WHERE 1=1
         """.format(colname, table)
 
-    first_condiction_flag = True
     if keywords:
-        first_condiction_flag, where_or_and = is_where_or_and(
-            first_condiction_flag
-        )
-
         keywords_statement = []
         for k in keywords.split(","):
             k = k.strip()
             keywords_statement.append(f" `keywords` like '%{k}%' ")
+
         keywords_statement = (
-            f"{where_or_and} ( " + "OR".join(keywords_statement) + " )"
+            "AND ( " + "OR".join(keywords_statement) + " )"
         )
         sql = f" {sql} {keywords_statement} "
 
     if positions:
-        first_condiction_flag, where_or_and = is_where_or_and(
-            first_condiction_flag
-        )
-
         position_statement = []
         for p in positions.split(","):
             p = p.strip()
             position_statement.append(f" `position` like '%{p}%'")
+
         position_statement = (
-            f"{where_or_and} ( " + " AND ".join(position_statement) + " )"
+            "AND ( " + " AND ".join(position_statement) + " )"
         )
         sql = f" {sql} {position_statement} "
 
     if volumeMin:
-        first_condiction_flag, where_or_and = is_where_or_and(
-            first_condiction_flag
-        )
-
-        volumeRange_statement = f"{where_or_and} `volume_now` >= {volumeMin}"
+        volumeRange_statement = f"AND `volume_now` >= {volumeMin}"
         sql = f" {sql} {volumeRange_statement} "
 
     if volumeMax:
-        first_condiction_flag, where_or_and = is_where_or_and(
-            first_condiction_flag
-        )
-
-        volumeRange_statement = f"{where_or_and} `volume_now` <= {volumeMax} "
+        volumeRange_statement = f"AND `volume_now` <= {volumeMax} "
         sql = f" {sql} {volumeRange_statement} "
 
     if colname != "COUNT(*)":
@@ -139,34 +116,19 @@ def create_pages_sql(
 ) -> str:
     # TODO: maybe news_id not show
     # colname = get_colname(table, database)
-    if datatype == "page":
-        colname = "*"
-        sql = get_keywords_page_sql(
-            colname,
-            table,
-            pageNo,
-            pageSize,
-            keywords,
-            positions,
-            volumeMin,
-            volumeMax,
-            orderby,
-            ordertype,
-        )
-    elif datatype == "count":
-        colname = "COUNT(*)"
-        sql = get_keywords_page_sql(
-            colname,
-            table,
-            pageNo,
-            pageSize,
-            keywords,
-            positions,
-            volumeMin,
-            volumeMax,
-            orderby,
-            ordertype,
-        )
+    colname = "COUNT(*)" if datatype == "count" else "*"
+    sql = get_keywords_page_sql(
+        colname,
+        table,
+        pageNo,
+        pageSize,
+        keywords,
+        positions,
+        volumeMin,
+        volumeMax,
+        orderby,
+        ordertype,
+    )
     return sql
 
 
